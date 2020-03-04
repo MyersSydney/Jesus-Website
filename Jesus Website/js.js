308ffca8b9c80a59696aced2248b3ddeec16bcd1
Sydney Myers <myerssm02@uww.edu>
2020-03-03 21:30:15 -0600

    doesnt work quite right yet

--- Jesus Website/js.js
+++ Jesus Website/js.js
@@ -10,13 +10,12 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+  var poses = document.getElementsByClassName("poses");
+  if (n > poses.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = poses.length}
+  for (var i = 0; i < poses.length; i++) {
+      poses[i].style.display = "none";  
   }
  
-  slides[slideIndex-1].style.display = "block";  
+  poses[slideIndex-1].style.display = "block";  
 }
