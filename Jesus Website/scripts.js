ebf31914fc19729dac3322b7b1b136bca013a802
Sydney Myers <myerssm02@uww.edu>
2020-03-31 22:15:25 -0500

    shirts

--- Jesus Website/scripts.js
+++ Jesus Website/scripts.js
@@ -1,3 +1,25 @@
 function defaultColors(){
 	document.getElementById('skin').style.fill='yellow';
 }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var poses = document.getElementsByClassName("poses");
+  if (n > poses.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = poses.length}
+  for (var i = 0; i < poses.length; i++) {
+      poses[i].style.display = "none";  
+  }
+ 
+  poses[slideIndex-1].style.display = "block";  
+}
