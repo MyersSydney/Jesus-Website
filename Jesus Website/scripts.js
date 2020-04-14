0796b49bd2b2e161882d387c8ebb621b5c51c4de
Sydney Myers <myerssm02@uww.edu>
2020-04-14 16:23:48 -0500

    0/10

--- Jesus Website/scripts.js
+++ Jesus Website/scripts.js
@@ -1,25 +1,48 @@
 function defaultColors(){
 	document.getElementById('skin').style.fill='yellow';
 }
+function pose1(){
+	document.getElementById('skin').style.opacity=100;
+	document.getElementById('hair').style.opacity=100;
+	document.getElementById('eyes').style.opacity=100;
+	document.getElementById('mouth').style.opacity=100;
+	document.getElementById('clothing').style.opacity=100;
+	document.getElementById('tie').style.opacity=100;
+	
+	document.getElementById('skin2').style.opacity=0;
+	document.getElementById('base2').style.opacity=0;
+	document.getElementById('hair2').style.opacity=0;
+	document.getElementById('eyes2').style.opacity=0;
+	document.getElementById('shirt2').style.opacity=0;
+	document.getElementById('shirt22').style.opacity=0;
+}
+function pose2(){
+	document.getElementById('skin').style.opacity=0;
+	document.getElementById('hair').style.opacity=0;
+	document.getElementById('eyes').style.opacity=0;
+	document.getElementById('mouth').style.opacity=0;
+	document.getElementById('clothing').style.opacity=0;
+	document.getElementById('tie').style.opacity=0;
+	
+	document.getElementById('skin2').style.opacity=100;
+	document.getElementById('base2').style.opacity=100;
+	document.getElementById('hair2').style.opacity=100;
+	document.getElementById('eyes2').style.opacity=100;
+	document.getElementById('shirt2').style.opacity=100;
+	document.getElementById('shirt22').style.opacity=100;
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function getElementsById(ids) {
+    var idList = ids.split(" ");
+    var results = [], item;
+    for (var i = 0; i < idList.length; i++) {
+        item = document.getElementById(idList[i]);
+        if (item) {
+            results.push(item);
+        }
+    }
+    return(results);
 }
 
-function showSlides(n) {
-  var poses = document.getElementsByClassName("poses");
-  if (n > poses.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = poses.length}
-  for (var i = 0; i < poses.length; i++) {
-      poses[i].style.display = "none";  
-  }
- 
-  poses[slideIndex-1].style.display = "block";  
-}
+
