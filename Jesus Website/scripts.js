faf6eb9627e68d28ce65d1370bc82f61b42a8a11
Sydney Myers <myerssm02@uww.edu>
2020-04-21 16:28:10 -0500

    did some stuff

--- Jesus Website/scripts.js
+++ Jesus Website/scripts.js
@@ -1,42 +1,23 @@
 function defaultColors(){
-	document.getElementById('skin').style.fill='yellow';
-	document.getElementById('hair2').style.fill = '4E311D';
-	document.getElementById('skin2').style.fill='E7B493';
-	document.getElementById('base2').style.fill='E7B493';
-	document.getElementById('shirt22').style.fill='DF5E53';
-	document.getElementById('eyes2').style.fill = '3C2E29';
+	document.getElementById('hair').style.fill = '4E311D';
+	document.getElementById('skin').style.fill='E7B493';
+	document.getElementById('base').style.fill='E7B493';
+	document.getElementById('shirt2').style.fill='DF5E53';
+	document.getElementById('shirt').style.fill = 'white';
+	document.getElementById('eyes').style.fill = '3C2E29';
 	}
-function pose1(){
-	document.getElementById('skin').style.opacity=100;
-	document.getElementById('hair').style.opacity=100;
-	document.getElementById('eyes').style.opacity=100;
-	document.getElementById('mouth').style.opacity=100;
-	document.getElementById('clothing').style.opacity=100;
-	document.getElementById('tie').style.opacity=100;
-	
-	document.getElementById('skin2').style.opacity=0;
-	document.getElementById('base2').style.opacity=0;
-	document.getElementById('hair2').style.opacity=0;
-	document.getElementById('eyes2').style.opacity=0;
-	document.getElementById('shirt2').style.opacity=0;
-	document.getElementById('shirt22').style.opacity=0;
-}
-function pose2(){
-	document.getElementById('skin').style.opacity=0;
-	document.getElementById('hair').style.opacity=0;
-	document.getElementById('eyes').style.opacity=0;
-	document.getElementById('mouth').style.opacity=0;
-	document.getElementById('clothing').style.opacity=0;
-	document.getElementById('tie').style.opacity=0;
-	
-	document.getElementById('skin2').style.opacity=100;
-	document.getElementById('base2').style.opacity=100;
-	document.getElementById('hair2').style.opacity=100;
-	document.getElementById('eyes2').style.opacity=100;
-	document.getElementById('shirt2').style.opacity=100;
-	document.getElementById('shirt22').style.opacity=100;
 
+var jesuses= ['jesus1', 'jesus2', 'jesus3'];
+function pose1(id){
+	document.getElementById(id).style.visibility = "visible";
+	
+	for(let i = 0; i < jesuses.length; i++){
+		if(jesuses[i] != id){
+			document.getElementById(jesuses[i]).style.visibility = "hidden";
+		}
+	}
 }
+
 
 function getElementsById(ids) {
     var idList = ids.split(" ");
@@ -49,5 +30,13 @@ function getElementsById(ids) {
     }
     return(results);
 }
+
+var scale = Math.min( 
+  availableWidth / contentWidth, 
+  availableHeight / contentHeight 
+);
+
+
+
 
 
